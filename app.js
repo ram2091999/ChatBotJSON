d918325d540cdc71108bfe6ba34c0bef436f2510
pavi114 <pavisri99@gmail.com>
2019-12-09 14:32:07 +0000

    Add Forms for Pending Requests and Others

--- app.js
+++ app.js
@@ -6,13 +6,10 @@ const path = require("path");
 const app = express();
 const fs = require("fs");
 let rawdata = fs.readFileSync(path.resolve(__dirname, "./public/data.json"));
-<<<<<<< HEAD
 
-=======
 let data = JSON.parse(rawdata);
 data = data["services offered"];
 console.log(data);
->>>>>>> df639421eb5c3e189b586fd06db52528ddce74c5
 
 app.use(
   bodyParser.urlencoded({
@@ -21,23 +18,23 @@ app.use(
 );
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname + "/index.html"));
 });
 
 
 
-app.post("/getElement",function(req,res){
-  let array=JSON.parse(req.body.flow);
-  let data=JSON.parse(rawdata)["services offered"];
+app.post("/getElement", function (req, res) {
+  let array = JSON.parse(req.body.flow);
+  let data = JSON.parse(rawdata)["services offered"];
   console.log(data);
-  for(var i=0;i<array.length;i++){
-    data=data["flow"][array[i]];
+  for (var i = 0; i < array.length; i++) {
+    data = data["flow"][array[i]];
 
   }
   res.send(data);
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Listening in port 3000");
 });
--- public/index.js
+++ public/index.js
@@ -7,63 +7,130 @@ const sendButton = document.getElementById("send-button");
 const main = document.getElementById("main");
 let flow = [];
 
-sendButton.addEventListener("click", function() {
-  $.ajax({
-    url: "getElement",
-    data: { flow: JSON.stringify(flow) },
+sendButton.addEventListener("click", function () {
+	$.ajax({
+		url: "getElement",
+		data: {
+			flow: JSON.stringify(flow)
+		},
 
-    type:"POST",
+		type: "POST",
 
-    success: function(result) {
-      // flow.push(result);
-      let current = `<div>
-            <h2>${result["name"]}</h2>
-            <p>${result["description"]}</p>
-            <ul>`;
+		success: function (result) {
+			// flow.push(result);
+			let current = `<div>
+			<h2>${result["name"]}</h2>
+			<p>${result["description"]}</p>
+			<ul>`;
 
 
-        if (result["type"] == "list") {
-          result["elements"].forEach(ele => current += `<li><button class="btn btn-outline-secondary" onclick="myFunction('${ele}')" >${ele}</button></li>`);
-        }
+			if (result["type"] == "list") {
+				result["elements"].forEach(ele => current += `<li><button class="btn btn-outline-secondary" onclick="myFunction('${ele}')" >${ele}</button></li>`);
+			}
 
-      current += "</ul></div>";
+			current += "</ul></div>";
 
-      main.innerHTML+=current;
+			main.innerHTML += current;
 
-    }
-  });
+		}
+	});
 });
 
 
-function myFunction(str){
-  flow = [...new Set(flow)];
+function myFunction(str) {
+	flow = [...new Set(flow)];
 
-  flow.push(str);
-  console.log(flow);
-  ajax(flow);
-
+	flow.push(str);
+	console.log(str);
+	ajax(flow);
+	if (str == "Pending Request Status") {
+		renderFormPending();
+	} else if (str.toLowerCase() == "other") {
+		renderFormOthers();
+	}
 }
 
-function ajax(flow){
-  $.ajax({
-    url: "getElement",
-    data: { flow: JSON.stringify(flow) },
-    type:"POST",
-    success: function(result) {
-      // flow.push(result);
-      let current = `<div>
-            <h2>${result["name"]}</h2>
+function ajax(flow) {
+	$.ajax({
+		url: "getElement",
+		data: {
+			flow: JSON.stringify(flow)
+		},
+		type: "POST",
+		success: function (result) {
+			// flow.push(result);
+			if (result.hasOwnProperty("name")) {
+				console.log(result)
+				let current = `<div>
+			<h2>${result["name"]}</h2>
+			
+			<ul>`;
 
-            <ul>`;
+				if (result["type"] == "list") {
+					result["elements"].forEach(ele => current += `<li><button class="btn btn-outline-secondary" onclick="myFunction('${ele}')" >${ele}</button></li>`);
+				}
 
-        if (result["type"] == "list") {
-          result["elements"].forEach(ele => current += `<li><button class="btn btn-outline-secondary" onclick="myFunction('${ele}')" >${ele}</button></li>`);
-        }
+				current += "</ul></div>";
 
-      current += "</ul></div>";
+				main.innerHTML += current;
+			} else {
+				main.innerHTML += renderFormOthers();
+			}
+		}
 
-      main.innerHTML+=current;
-    }
-  });
-
+	});
 }
+
+let renderFormPending = () => {
+	main.innerHTML += `<form action="" method="post">
+	<div class="form-group">
+	<label for="name">Name:</label>
+	<input type="text" class="form-control" id="name">
+	</div>
+	<div class="form-group">
+	<label for="phone">Phone Number</label>
+	<input type="number" class="form-control" id="phone">
+	</div>
+	<div class="form-group">
+	<label for="complaintNum">Complaint Number</label>
+	<input type="number" class="form-control" id="complaintNum">
+	</div>
+	<div class="form-group">
+	<label for="email">Email</label>
+	<input type="email" class="form-control" id="email">
+	</div>
+	</form>`
+}
+
+let renderFormOthers = () => {
+	return `<form action="" method="post">
+	<div class="form-group">
+	<label for="name">Name:</label>
+	<input type="text" class="form-control" id="name">
+	</div>
+	<div class="form-group">
+	<label for="email">Email</label>
+	<input type="email" class="form-control" id="email">
+	</div>
+	<div class="form-group">
+	<label for="description">Describe</label>
+	<textarea class="form-control" width="40" id="description"></textarea>
+	</div>
+	</form>`
+}
+
+// let renderFormService = () => {
+// 	return `<form action="" method="post">
+// 	<div class="form-group">
+// 	<label for="name">Name:</label>
+// 	<input type="text" class="form-control" id="name">
+// 	</div>
+// 	<div class="form-group">
+// 	<label for="email">Email</label>
+// 	<input type="email" class="form-control" id="email">
+// 	</div>
+// 	<div class="form-group">
+// 	<label for="email">Email</label>
+// 	<input type="email" class="form-control" id="email">
+// 	</div>`;
+// }
