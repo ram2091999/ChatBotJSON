1f2638508a8caf15684fb40d9c1cb757dd8b6041
pavi114 <pavisri99@gmail.com>
2019-12-09 12:39:00 +0000

    Front End

--- public/index.js
+++ public/index.js
@@ -1,6 +1,30 @@
 //jshint esversion:6
 
-const messageUL=document.getElementById("message-box");
-const activeElement=document.getElementById("active-element");
-const input=document.getElementById("input-box");
-const sendButton=document.getElementById("send-button");
+const messageUL = document.getElementById("message-box");
+const activeElement = document.getElementById("active-element");
+const input = document.getElementById("input-box");
+const sendButton = document.getElementById("send-button");
+const main = document.getElementById("main");
+let flow = [];
+
+sendButton.addEventListener("click", function() {
+  $.ajax({
+    url: "getElement.js",
+    data: { flow: JSON.stringify(flow) },
+    success: function(result) {
+      flow.push(result);
+      let current = `<div> 
+            <h2>${result["name"]}</h2>
+            <p>${result["description"]}</p>
+            <ul>`;
+
+      //   if (result["type"] == "list") {
+      //     result["element"].forEach(ele => current += `<li>${ele}</li>`);
+      //   }
+
+      current += "</ul></div>";
+
+      main.appendChild(current);
+    }
+  });
+});
